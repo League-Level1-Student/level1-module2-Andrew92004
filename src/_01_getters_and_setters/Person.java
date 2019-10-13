e6b8fa959df97e510fbaa73052acb38678e2221a
Andrew J Zhang <andrewjiazhangwestview@gmail.com>
2019-10-13 09:43:07 -0700

    started module 2

---
+++ src/_01_getters_and_setters/Person.java
@@ -0,0 +1,23 @@
+package _01_getters_and_setters;
+
+public class Person {
+private String name;
+private String superpower;
+
+public String toString() {
+	return getName() + " has mad " + getSuperpower() + " skills.";
+}
+public String getName() {
+	return name;
+}
+public String getSuperpower() {
+	return superpower;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public void setSuperpower(String superpower) {
+	this.superpower = superpower;
+}
+
+}
---
+++ src/_01_getters_and_setters/Runner.java
@@ -0,0 +1,21 @@
+package _01_getters_and_setters;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		Person person1 = new Person();
+		Person person2 = new Person();
+		Person person3 = new Person();
+		person1.setSuperpower("flying");
+		person1.setName("Steve");
+		person2.setSuperpower("super strength");
+		person2.setName("Arnold");
+		person3.setSuperpower("teleportation");
+		person3.setName("Kody");
+		System.out.println(person1.toString());
+		System.out.println(person2.toString());
+		System.out.println(person3.toString());
+	}
+
+}
